64ae64e0f5f51b2dd888d63c3f7a285165545b51
AaryaKumar1 <73297516+AaryaKumar1@users.noreply.github.com>
2020-12-29 12:41:47 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['UName'];
+       $Email = $_POST['Email'];
+       $Subject = $_POST['Subject'];
+       $Msg = $_POST['msg'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.php?error');
+       }
+       else
+       {
+           $to = "aarya.kumar@globalschoolsalumni.org";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index.php?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:index.php");
+    }
+?>
+<?php
+    $Msg = "";
+    if(isset($_GET['error']))
+    {
+        $Msg = " Please Fill in the Blanks ";
+        echo '<div class="alert alert-danger">'.$Msg.'</div>';
+    }
+
+    if(isset($_GET['success']))
+    {
+        $Msg = " Your Message Has Been Sent ";
+        echo '<div class="alert alert-success">'.$Msg.'</div>';
+    }
+
+?>
